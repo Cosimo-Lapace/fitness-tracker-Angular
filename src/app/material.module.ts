c84c4c4c1cbca996cd5d5c48534daa4f3955b43b
CosimoLP <cosimolapace@libero.it>
2023-07-11 17:19:58 +0200

    component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,24 @@
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  imports: [MatIconModule, MatButtonModule],
-  exports: [MatIconModule, MatButtonModule],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
+  exports: [
+    MatIconModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
 })
 export class MaterialModule {}
