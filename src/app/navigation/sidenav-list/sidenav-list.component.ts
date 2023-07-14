2166a33bb5c5e89c73f01037cb077f5b62c35df8
CosimoLP <cosimolapace@libero.it>
2023-07-14 11:10:29 +0200

    slitting sidenav

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav-list',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidenav-list.component.css']
 })
 export class SidenavListComponent {
-
+@Input()sidenav?:any
 }
